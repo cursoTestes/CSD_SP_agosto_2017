d6d3efa74ad20ec2627ea9ccc9555fc291ae4975
cursoTestes <cfc@ufrj.br>
2017-08-24 15:38:49 -0300

    dojo de banco de dados

---
+++ bancoDados/src/test/java/br/com/k21/VendaDAOTest.java
@@ -0,0 +1,50 @@
+package br.com.k21;
+
+import org.junit.Test;
+
+import br.com.k21.dao.VendaDAO;
+import br.com.k21.infra.BaseDBTest;
+import junit.framework.Assert;
+
+public class VendaDAOTest extends BaseDBTest {
+
+	@Test
+	public void testTotalVendasParaVendedorComVendasEmAnosDiferentes() {
+		// Arrange
+		Vendedor vendedor = new Vendedor();
+		int entradaIdVendedor = 2;
+		vendedor.setId(entradaIdVendedor);
+		int entradaAno = 2011;
+		double resultado;
+		double esperado = 600.0;
+		
+		// act
+		VendaDAO.setEntityManager(emf.createEntityManager());
+		
+		resultado = VendaDAO.buscarTotalDeVendasPorVendedorEAno(vendedor, entradaAno);
+
+		// asserts
+		assertEquals(esperado, resultado);
+	}
+	
+	
+	@Test
+	public void testVendedorSemVenda() {
+		// Arrange
+		Vendedor vendedor = new Vendedor();
+		int entradaIdVendedor = 2;
+		vendedor.setId(entradaIdVendedor);
+		int entradaAno = 2001;
+		
+		// act
+		VendaDAO.setEntityManager(emf.createEntityManager());
+		try {
+			VendaDAO.buscarTotalDeVendasPorVendedorEAno(vendedor, entradaAno);
+			fail();
+		} catch (RuntimeException e) {
+			Assert.assertEquals("Sem venda", e.getMessage());
+		}
+	}
+	
+
+}
